c4b684c5c802a06406eb5489608e242a2043e4c9
gvsnprasad <105455572+gvsnprasad@users.noreply.github.com>
2022-05-12 15:15:07 -0400

    commited changes with classname please review

--- src/main/java/com/example/helloworldgit/HelloWorldController1.java
+++ src/main/java/com/example/helloworldgit/HelloWorldController1.java
@@ -10,7 +10,7 @@ public class HelloWorldController1 {
     @GetMapping(value = "/helloMessage1/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String HelloMessage1(@PathVariable String name){
 
-        return "Hello from helloworld1from gvsn2account on class HelloWorldController1" + name;
+        return "Hello from helloworld1from gvsn2account on class HelloWorldController1. " + name;
 
     }
 
